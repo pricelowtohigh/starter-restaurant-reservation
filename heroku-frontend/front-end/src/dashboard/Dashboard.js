8e645bf5dc0aaca8b3a4b02f410cf780648475c2
Dane <danefalberg@gmail.com>
2022-11-01 15:13:01 -0400

    Add 'heroku-frontend/' from commit '30e5e96c7ac3602d139468fed80c016a8f5691c0'
    git-subtree-dir: heroku-frontend
    git-subtree-mainline: 30e5e96c7ac3602d139468fed80c016a8f5691c0
    git-subtree-split: 30e5e96c7ac3602d139468fed80c016a8f5691c0

---
+++ heroku-frontend/front-end/src/dashboard/Dashboard.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import useQuery from "../utils/useQuery";
+import { listReservations, listTables } from "../utils/api";
+import { today, previous, next } from "../utils/date-time";
+import ReservationsList from "../reservations/ReservationsList";
+import TablesList from "../tables/TablesList";
+import ErrorAlert from "../layout/ErrorAlert";
+import { useHistory } from "react-router";
+
+/**
+ * Defines the dashboard page.
+ * @param date
+ *  the date for which the user wants to view reservations.
+ * @returns {JSX.Element}
+ */
+
+function Dashboard({ date }) {
+  const history = useHistory();
+  const query = useQuery();
+  date = query.get("date") || date;
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
+
+  useEffect(loadDashboard, [date]);
+
+  function loadDashboard() {
+    const abortController = new AbortController();
+    setReservationsError(null);
+    listReservations({ date }, abortController.signal)
+      .then(setReservations)
+      .catch(setReservationsError);
+    listTables(abortController.signal).then(setTables).catch(setTablesError);
+    return () => abortController.abort();
+  }
+
+  return (
+    <main>
+      <div>
+        <h1>Dashboard</h1>
+        <ErrorAlert error={reservationsError} />
+        <div>
+          <h4>Reservations for {date}</h4>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => history.push(`/dashboard?date=${previous(date)}`)}
+          >
+            Previous Date
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => history.push(`/dashboard?date=${today()}`)}
+          >
+            Today
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => history.push(`/dashboard?date=${next(date)}`)}
+          >
+            Next Date
+          </button>
+        </div>
+        <div>
+          <ReservationsList reservations={reservations} />
+        </div>
+        <ErrorAlert error={tablesError} />
+        <div>
+          <h4>Tables</h4>
+        </div>
+        <div>
+          <TablesList tables={tables} />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Dashboard;
---
+++ heroku-frontend/heroku-backend/back-end/src/tables/tables.controller.js
@@ -0,0 +1,182 @@
+const tablesService = require("./tables.service");
+const reservationsService = require("../reservations/reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+// Validation middlewares //
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`,
+    });
+  };
+}
+
+function tableNamePropertyIsValid(req, res, next) {
+  const { table_name } = req.body.data;
+  if (table_name.length > 1) {
+    return next();
+  } else {
+    return next({
+      status: 400,
+      message: `'table_name' is invalid. Table Name must be at least two characters long.`,
+    });
+  }
+}
+
+function capacityPropertyIsValid(req, res, next) {
+  const { capacity } = req.body.data;
+  if (typeof capacity === "number" && capacity > 0) {
+    return next();
+  } else {
+    return next({
+      status: 400,
+      message: `'capacity' is invalid. Capacity must be at least 1.`,
+    });
+  }
+}
+
+async function tableExists(req, res, next) {
+  const tableId = req.params.table_id;
+  const existingTable = await tablesService.readTable(tableId);
+  if (existingTable) {
+    res.locals.table = existingTable;
+    return next();
+  } else {
+    return next({
+      status: 404,
+      message: `Table ${tableId} does not exist.`,
+    });
+  }
+}
+
+async function reservationExists(req, res, next) {
+  const reservationId = req.body.data.reservation_id;
+  const existingReservation = await reservationsService.readReservation(
+    reservationId
+  );
+  if (existingReservation) {
+    res.locals.reservation = existingReservation;
+    return next();
+  } else {
+    return next({
+      status: 404,
+      message: `Reservation ${reservationId} does not exist.`,
+    });
+  }
+}
+
+function tableHasCapacity(req, res, next) {
+  const table = res.locals.table;
+  const reservation = res.locals.reservation;
+  if (table.capacity >= reservation.people) {
+    return next();
+  } else {
+    return next({
+      status: 400,
+      message: `This table does not have the capacity for this reservation.`,
+    });
+  }
+}
+
+function tableIsAvailable(req, res, next) {
+  const tableIsSeated = res.locals.table.reservation_id;
+  if (!tableIsSeated) {
+    return next();
+  } else {
+    return next({
+      status: 400,
+      message: `This table is occupied.`,
+    });
+  }
+}
+
+function tableIsOccupied(req, res, next) {
+  const tableIsSeated = res.locals.table.reservation_id;
+  if (tableIsSeated) {
+    return next();
+  } else {
+    return next({
+      status: 400,
+      message: `This table is not occupied.`,
+    });
+  }
+}
+
+function reservationIsNotAlreadySeated(req, res, next) {
+  const status = res.locals.reservation.status;
+  if (status !== "seated") {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: `This reservation is already seated.`,
+    });
+  }
+}
+
+// HTTP request handlers for 'tables' resources //
+
+async function createTable(req, res) {
+  const newTable = req.body.data;
+  const responseData = await tablesService.createTable(newTable);
+  res.status(201).json({ data: responseData });
+}
+
+async function readTable(req, res) {
+  const tableId = req.params.table_id;
+  const responseData = await tablesService.readTable(tableId);
+  res.status(200).json({ data: responseData });
+}
+
+async function seatTable(req, res) {
+  const tableId = res.locals.table.table_id;
+  const reservationId = res.locals.reservation.reservation_id;
+  const responseData = await tablesService.seatTable(tableId, reservationId);
+  res.status(200).json({ data: responseData });
+}
+
+async function unseatTable(req, res) {
+  const tableId = req.params.table_id;
+  const reservationId = res.locals.table.reservation_id;
+  const responseData = await tablesService.unseatTable(tableId, reservationId);
+  res.status(200).json({ data: responseData });
+}
+
+async function listTables(req, res) {
+  const responseData = await tablesService.listTables();
+  res.status(200).json({ data: responseData });
+}
+
+// Exports //
+
+module.exports = {
+  createTable: [
+    bodyDataHas("table_name"),
+    bodyDataHas("capacity"),
+    tableNamePropertyIsValid,
+    capacityPropertyIsValid,
+    asyncErrorBoundary(createTable),
+  ],
+  readTable: [asyncErrorBoundary(tableExists), asyncErrorBoundary(readTable)],
+  seatTable: [
+    bodyDataHas("reservation_id"),
+    asyncErrorBoundary(tableExists),
+    asyncErrorBoundary(reservationExists),
+    reservationIsNotAlreadySeated,
+    tableIsAvailable,
+    tableHasCapacity,
+    asyncErrorBoundary(seatTable),
+  ],
+  unseatTable: [
+    asyncErrorBoundary(tableExists),
+    tableIsOccupied,
+    asyncErrorBoundary(unseatTable),
+  ],
+  listTables: [asyncErrorBoundary(listTables)],
+};
