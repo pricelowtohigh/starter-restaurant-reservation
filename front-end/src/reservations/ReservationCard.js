e8fbc0a3bc528475599341acdf1bdb7b527a8885
Dane <danefalberg@gmail.com>
2022-11-24 11:50:40 -0500

    chasing vercel errors

--- front-end/src/reservations/ReservationCard.js
+++ front-end/src/reservations/ReservationCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function ReservationCard({ reservation }) {
