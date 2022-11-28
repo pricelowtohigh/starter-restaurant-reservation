f87b4dd851aacf1ea31e240ed2d2c207be1420f8
Dane <danefalberg@gmail.com>
2022-11-28 13:27:34 -0500

    removed heroku files

--- heroku-frontend/back-end/src/db/seeds/00-reservations.js
+++
@@ -1,8 +0,0 @@
-const reservations = require("../seeds/00-reservations.json");
-
-exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
-  .then(() => {
-    return knex("reservations").insert(reservations)
-  });
-};
--- heroku-frontend/heroku-backend/front-end/src/tables/TableOptions.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TableOptions({ table }) {
-  return (
-    <option value={table.table_id}>
-      {table.table_name} - {table.capacity}
-    </option>
-  );
-}
