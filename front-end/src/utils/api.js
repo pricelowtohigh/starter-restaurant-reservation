90191440b0783402383380f0efd9e1dd67492d8f
Dane <danefalberg@gmail.com>
2022-11-30 20:52:06 -0500

    CORS troubleshooting

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,8 @@ import axios from "axios";
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = "https://restaurant-reservation-backend-muq2.onrender.com";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-backend-muq2.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
