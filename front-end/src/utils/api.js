bb039781b615d108d1d1d7c127f528d1690e072a
Dane <danefalberg@gmail.com>
2022-11-30 20:29:53 -0500

    Redeploy using Render

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import axios from "axios";
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-backend-muq2.onrender.com/";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
